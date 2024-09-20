a63a8315026fbfb776095cf02eb95c22b414af14
janslan <janko.slanina@hotmail.com>
2024-09-20 19:46:09 +0100

    q

--- WinCalc/MainWindow.xaml.cs
+++ WinCalc/MainWindow.xaml.cs
@@ -292,7 +292,7 @@ namespace WinCalc
                 this.mathOperation("ReturnValue");
             }
         }
-        // Figure out why the sidebar is fucking huge. 
+        // Figure out why the sidebar is fucking huge. L
         public void mathOperation(string input) 
         {
             if (input == "N/A")
